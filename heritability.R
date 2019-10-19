8992e6b353151467211b8dc2dce2ff11936f3207
Cristian Groza <cristian.groza@protonmail.com>
2019-10-18 21:11:31 -0400

    Heritability.

--- heritability.R
+++ heritability.R
@@ -3,8 +3,8 @@ library(tidyverse)
 library(ggplot2)
 library(car)
 
-betas <- read_tsv("plink2.PHENO1.glm.logistic", col_names = T)
-freqs <- read_tsv("plink2.afreq", col_names = T) %>% filter(ID %in% betas$ID)
+betas <- read_table("plink.assoc.logistic", col_names = T)
+freqs <- read_tsv("plink2.afreq", col_names = T) %>% filter(ID %in% betas$SNP)
 
-freqs <- freqs %>% mutate(P = betas$P, beta.sq = betas$OR^2, HERIT = 2* ALT_FREQS * (1 - ALT_FREQS) * beta.sq)
+freqs <- freqs %>% mutate(P = betas$P, beta.sq = betas$BETA^2, HERIT = 2* ALT_FREQS * (1 - ALT_FREQS) * beta.sq)
 
