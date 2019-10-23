a1a6b55bde3d047bea7aac64bd65e98dfd9c8610
Cristian Groza <cristian.groza@protonmail.com>
2019-10-23 14:21:14 -0400

    Corrected heritabilities

--- heritability.R
+++ heritability.R
@@ -8,9 +8,18 @@ sig.snps <- filter(adj.p, BONF < 0.05)
 
 freqs <- read_tsv("plink2.afreq", col_names = T) %>% filter(ID %in% betas$SNP)
 
-freqs <- freqs %>% mutate(P = betas$P, beta.sq = betas$BETA^2, HERIT = 2* ALT_FREQS * (1 - ALT_FREQS) * beta.sq, Bonferroni = if_else(ID %in% sig.snps$SNP, "Significant", "Non-significant"))
+K = 0.00052
+
+freqs <- freqs %>% mutate(P = betas$P,
+                          beta.sq = betas$BETA^2,
+                          UNADJ_HERIT = 2* ALT_FREQS * (1 - ALT_FREQS) * beta.sq,
+                          Bonferroni = if_else(ID %in% sig.snps$SNP, "Significant", "Non-significant"),
+                          ADJ_FACT = ((K*(1-K))^2)/(dnorm(qnorm(K))^2*0.5*(1-0.5)),
+                          HERIT = UNADJ_HERIT * ADJ_FACT)
 
 ggplot(freqs) + geom_jitter(aes(x="SNP", y=log10(HERIT), color = Bonferroni), width = 1) +  theme_bw() + theme(legend.position = "top") + ggtitle("Heritability across SNPs") +
-  xlab("") + ylab("log10(SNP heritability)") 
+  xlab("") + ylab("log10(SNP heritability)")
 
 ggsave("snp-heritability.png")
+
+freqs %>% filter(Bonferroni=="Significant") %>% summarise(TOTAL=sum(HERIT))
