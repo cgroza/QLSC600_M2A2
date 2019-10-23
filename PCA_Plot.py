86251891102af6980fde93dadf90168e2c8ccfc5
Alaa Abdelgawad <alaaabdelgawad97@gmail.com>
2019-10-23 13:16:35 -0400

    adding new script

--- PCA_Plot.py
+++ PCA_Plot.py
@@ -12,13 +12,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #reading data
-vector = pd.read_csv('plink2.txt', delimiter="\t")
-vector1 =pd.read_csv('eigenvec.txt',delimiter=" ", header= None)
+vector = pd.read_csv('eigenvec1000.txt', delimiter=" ", header= None)
+vector1 =pd.read_csv('eigenvec25000.txt',delimiter=" ", header= None)
+vector=vector.iloc[:,0:4]
 vector1=vector1.iloc[:,0:4]
 diag = pd.read_csv('plink-MS.txt',delimiter=" ", header= None)
 #changing col names
 diag.columns = ['indx','1st','2nd','3rd','4th','MS']
-vector.rename(columns={'#FID':'indx'},inplace=True)
+vector.columns=['indx','count','PC1','PC2']
 vector1.columns=['indx','count','PC1','PC2']
 #merging two dataframe
 combine= pd.merge(vector, diag, on="indx")
@@ -53,7 +54,7 @@ plt.xlabel('pc1',weight='bold')
 plt.ylabel('pc2',weight='bold')
 plt.legend(loc='best') 
 plt.show()
-fig.savefig('plot4.png')
+fig.savefig('plot1000.png')
 
 
 fig2 = plt.figure() 
@@ -68,7 +69,7 @@ plt.ylabel('pc2',weight='bold')
 plt.legend(loc='best') 
 plt.xlim((-0.04,0.01))
 plt.show()
-fig2.savefig('plot1.png')
+fig2.savefig('plot25000.png')
 
 '''
 vector1 = vector[(x>-0.02)]
@@ -85,3 +86,16 @@ plt.ylabel('pc2',weight='bold')
 plt.show()
 fig2.savefig('plot2.png')
 '''
+import matplotlib.pyplot as plt
+labels = '1b', '1d', '1f', '4','5','6','No Data'
+sizes = [2,2,23,2,3,25,2]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue','blue','brown','gray']
+explode = (0.1, 0.1, 0.1, 0, 0, 0, 0)  # explode 1st slice
+
+# Plot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
+
+plt.show()
